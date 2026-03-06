51cbf68e9fef5f04c1f8b307119075240c4324ef
Renze Nicolai <renze@rnplus.nl>
2026-03-06 22:37:19 +0100

    Update to ESP-IDF v5.5.1 and BSP v0.2.0

--- main/src/sid/i2s.cpp
+++ main/src/sid/i2s.cpp
@@ -19,7 +19,7 @@ esp_err_t I2S::init()
     // TODO: Move to a better place.
     ESP_LOGI(TAG, "Initializing BSP audio interface");
     bsp_audio_set_volume(0);
-    esp_err_t res = bsp_audio_initialize();
+    esp_err_t res = bsp_audio_initialize((uint32_t)DEFAULT_SAMPLERATE);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Initializing audio failed");
         return res;
@@ -30,44 +30,9 @@ esp_err_t I2S::init()
     bsp_audio_set_amplifier(false);
 
     ESP_LOGI(TAG, "Initializing I2S audio interface");
-    // I2S audio
-    chan_cfg = (i2s_chan_config_t)I2S_CHANNEL_DEFAULT_CONFIG(I2S_NUM_0, I2S_ROLE_MASTER);
-
-    res = i2s_new_channel(&chan_cfg, &i2s_handle, NULL);
+    res = bsp_audio_get_i2s_handle(&i2s_handle);
     if (res != ESP_OK) {
         ESP_LOGE(TAG, "Initializing I2S channel failed");
-        return res;
-    }
-
-
-    i2s_std_config_t i2s_config = {
-        .clk_cfg  = I2S_STD_CLK_DEFAULT_CONFIG((uint32_t)DEFAULT_SAMPLERATE),
-        .slot_cfg = I2S_STD_MSB_SLOT_DEFAULT_CONFIG(I2S_DATA_BIT_WIDTH_16BIT, I2S_SLOT_MODE_STEREO),
-        .gpio_cfg =
-            {
-                .mclk = GPIO_NUM_30,
-                .bclk = GPIO_NUM_29,
-                .ws   = GPIO_NUM_31,
-                .dout = GPIO_NUM_28,
-                .din  = I2S_GPIO_UNUSED,
-                .invert_flags =
-                    {
-                        .mclk_inv = false,
-                        .bclk_inv = false,
-                        .ws_inv   = false,
-                    },
-            },
-    };
-
-    res = i2s_channel_init_std_mode(i2s_handle, &i2s_config);
-    if (res != ESP_OK) {
-        ESP_LOGE(TAG, "Configuring I2S channel failed");
-        return res;
-    }
-
-    res = i2s_channel_enable(i2s_handle);
-    if (res != ESP_OK) {
-        ESP_LOGE(TAG, "Enabling I2S channel failed");
         return res;
     }
 
